3c0b1b7cfb61b0d0ba99dbe0a8db70ae52c96922
devshaffan <devshaffan@gmail.com>
2021-11-21 18:34:40 +0500

    Updates

---
+++ precache-manifest.5914c76e4dba56e1db9082f25804bb03.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "78cef7126ce25d7f8190",
+    "url": "/urdu_subtraction_numberLine/static/css/main.1ce5387c.chunk.css"
+  },
+  {
+    "revision": "78cef7126ce25d7f8190",
+    "url": "/urdu_subtraction_numberLine/static/js/main.c2060001.chunk.js"
+  },
+  {
+    "revision": "842719f777670f8d952e",
+    "url": "/urdu_subtraction_numberLine/static/js/runtime~main.f2003dba.js"
+  },
+  {
+    "revision": "394f09ee1c673203c3cf",
+    "url": "/urdu_subtraction_numberLine/static/css/2.3ac11d54.chunk.css"
+  },
+  {
+    "revision": "394f09ee1c673203c3cf",
+    "url": "/urdu_subtraction_numberLine/static/js/2.dd886786.chunk.js"
+  },
+  {
+    "revision": "212c4bcb95a3ce810e745bff36eb4bf4",
+    "url": "/urdu_subtraction_numberLine/static/media/movingFishR.212c4bcb.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/urdu_subtraction_numberLine/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "48d2eef01cecd55520ff1bf10c3e3748",
+    "url": "/urdu_subtraction_numberLine/static/media/movingFishL.48d2eef0.gif"
+  },
+  {
+    "revision": "a989d2c605289d1ad02c3ab3f3dd0981",
+    "url": "/urdu_subtraction_numberLine/static/media/more.a989d2c6.png"
+  },
+  {
+    "revision": "6f40b925808bcfceef28ee492480e926",
+    "url": "/urdu_subtraction_numberLine/static/media/less.6f40b925.png"
+  },
+  {
+    "revision": "5f1c11259cb15bc3800a9bb43a4a7242",
+    "url": "/urdu_subtraction_numberLine/static/media/background.5f1c1125.gif"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/urdu_subtraction_numberLine/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/urdu_subtraction_numberLine/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/urdu_subtraction_numberLine/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/urdu_subtraction_numberLine/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/urdu_subtraction_numberLine/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/urdu_subtraction_numberLine/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/urdu_subtraction_numberLine/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/urdu_subtraction_numberLine/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/urdu_subtraction_numberLine/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/urdu_subtraction_numberLine/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/urdu_subtraction_numberLine/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/urdu_subtraction_numberLine/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/urdu_subtraction_numberLine/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/urdu_subtraction_numberLine/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "d9e398530b7031d5833f171d8888fc56",
+    "url": "/urdu_subtraction_numberLine/static/media/numberLine.d9e39853.png"
+  },
+  {
+    "revision": "ca4158825f66b0587acca1862b795eaf",
+    "url": "/urdu_subtraction_numberLine/static/media/numberLine2.ca415882.png"
+  },
+  {
+    "revision": "59843b2d65b09b144fd402b9327e37d1",
+    "url": "/urdu_subtraction_numberLine/static/media/instructions.59843b2d.gif"
+  },
+  {
+    "revision": "b7a86e01fda95eec7f15889cba42c06c",
+    "url": "/urdu_subtraction_numberLine/index.html"
+  }
+];
